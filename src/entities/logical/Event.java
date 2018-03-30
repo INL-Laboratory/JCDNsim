fb86865f2bd7ea06ef3730e2629ea876e8d4b984
hamidgh09 <hamid.gh09@gmail.com>
2018-03-30 10:46:43 +0430

    test

---
+++ src/entities/logical/Event.java
@@ -0,0 +1,33 @@
+package entities.logical;
+
+import entities.physical.EndDevice;
+
+public class Event <T extends IEventHandler> {
+    private EventType type;
+    private T relatedEntity;
+    private float time;
+
+    public EventType getType() {
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
+    }
+
+    public T getRelatedEntity() {
+        return relatedEntity;
+    }
+
+    public void setRelatedEntity(T relatedEntity) {
+        this.relatedEntity = relatedEntity;
+    }
+
+    public float getTime() {
+        return time;
+    }
+
+    public void setTime(float time) {
+        this.time = time;
+    }
+}

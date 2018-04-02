9f817dde6293a5cb4c1d994f09aa73c6addd3c3a
Saeedhd <saeedhd@ce.sharif.edu>
2018-04-02 19:46:50 +0430

    Logger built.

--- src/entities/utilities/logger/Logger.java
+++ src/entities/utilities/logger/Logger.java
@@ -1,5 +1,23 @@
 package entities.utilities.logger;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Logger {
-    
+    private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
+    private static LocalDateTime now = LocalDateTime.now();
+    public static void print(String toPrint, float time){
+        System.out.println(toPrint);
+    }
+    public static void printWithClock(String toPrint, float time){
+
+        print(getRealTime()+ " " + toPrint, time);
+    }
+
+    public static String getRealTime(){
+        return dtf.format(now);
+    }
+
+    public static void main(String[] args) {
+    }
+
 }

915a3624c208b69bee5cf44d1022ea5b1065d57c
Saeedhd <saeedhd@ce.sharif.edu>
2018-11-10 16:49:24 +0330

    Client

--- src/entities/physical/Client.java
+++ src/entities/physical/Client.java
@@ -8,7 +8,8 @@ import java.util.List;
 
 
 public class Client extends EndDevice {
-//    public static int generatedId = 0;         //In order to have unique IDs I changed this to static.
+//    public static int generatedId = 0;
+// In order to have unique IDs I changed this to static.
 
 
     private Link link;
@@ -45,6 +46,17 @@ public class Client extends EndDevice {
 
 
 //    public static double totalTimeINClientHandleEvent = 0;
+
+
+    /**
+     * The events involving a client are sent here through below method. Possible course of actions:
+     * 1. sendReq: When a client intends to send a request
+     * 2. receiveSegment: When a segment was recognized to be associated with this entity
+     * 3. timeOut: When the response for a request is not received after a certain time,
+     * it will time out and added to the list of not received responses.
+     * @param event The related event to this entity
+     * @throws Exception
+     */
     @Override
     public void handleEvent(Event event) throws Exception {
 //        double tempTime = System.currentTimeMillis();
@@ -66,6 +78,11 @@ public class Client extends EndDevice {
 //        totalTimeINClientHandleEvent +=System.currentTimeMillis()-tempTime;
     }
 
+    /**
+     * It sends a request for a file.The file and the time is set by another class and sent in an event package to this client
+     * @param time The time the clients want to send
+     * @param fileID The desired file
+     */
     private void sendFileRequest(float time, int fileID) {
         Server dstServer = (Server)link.getOtherEndPoint(this);
         generateId();
@@ -86,7 +103,7 @@ public class Client extends EndDevice {
 
     }
 
-    public int generateId(){
+    private int generateId(){
         algorithmData.generatedId++;
         return algorithmData.generatedId;
     }
@@ -97,13 +114,20 @@ public class Client extends EndDevice {
         return  link.equals(this.link);
     }
 
+
+    /**
+     * This method override its father in EndDevice.
+     * @param time The time segment received
+     * @param segment The received segment
+     * @throws Exception: When an unexpected packet type is received.
+     */
     @Override
-    protected void parseReceivedSegment(float time, Segment segment) throws Exception {
+    protected void parseReceivedSegment(float time, Segment segment) throws OkayException {
 //        Logger.print(segment+ " is being parsed by " + this, time);
         if (isThisDeviceDestined(segment)) {
             switch (segment.getSegmentType()) {
                 case Data:
-                    checkTheReceivedFile(time, segment);
+                    checkTheReceivedData(time, segment);
                     break;
                 case Request:
                 default:
@@ -114,7 +138,14 @@ public class Client extends EndDevice {
         }
     }
 
-    private void checkTheReceivedFile(float time, Segment segment) throws Exception {
+
+    /**
+     * It checks the received data and if it's containing a file being waited it will submit the records.
+     * @param time
+     * @param segment
+     * @throws OkayException When the response is received after being timed out
+     */
+    private void checkTheReceivedData(float time, Segment segment) throws OkayException {
         IFile receivedFile = (IFile) segment.getOptionalContent();
         int requestID = segment.getId();
         int receivedFileID = receivedFile .getId();
@@ -122,7 +153,7 @@ public class Client extends EndDevice {
         Integer requestedFileId = this.sentRequestsFileId.get(requestID);
         if (sendTime==null || requestedFileId==null || requestedFileId != receivedFileID)   throw new OkayException(this + " received unrelated "+ receivedFile+ " in " + segment,time);
         if (DefaultValues.IS_TIME_OUT_ACTIVATED && time-sendTime>= DefaultValues.TIME_OUT)  {
-            throw new OkayException(this + "says to file " + requestedFileId +" in " + segment + " : Amadi Janam Beghorbanat Vali hala chera", time);
+            throw new OkayException(this + "says to file " + requestedFileId +" in " + segment + " : Thanks for coming, but it's late.", time);
         }
         servedRequestsTime.put(requestID,time);
         servedRequestsCost.put(requestID, segment.getToleratedCost());
